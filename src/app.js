5dc421fe7caf707a1b818f30e7d20f9e502e9597
Laura Montes <laumoen@gmail.com>
2025-06-02 11:11:56 +0000

    firstcommit

--- src/app.js
+++ src/app.js
@@ -5,10 +5,14 @@ window.onload = function() {
   
   let palo = ['♠', '♥', '♦', '♣'];
     let símbolo = Math.floor(Math.random() * palo.length);
-  document.querySelector("#randomSymbol").innerHTML = palo[símbolo] + número[cantidad];
+  document.querySelector("#randomSuit").innerHTML = palo[símbolo];
+
+};
+
+window.onload = function() {
 
   let número = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
     let cantidad = Math.floor(Math.random() * número.length);
   document.querySelector("#randomNumber").innerHTML = número[cantidad];
 
-};
+}
