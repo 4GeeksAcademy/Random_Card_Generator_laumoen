dcbe988865a47429e60445d388109fa86abc2db2
Laura Montes <laumoen@gmail.com>
2025-06-02 11:00:38 +0000

    firstcommit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  
+  let palo = ['♠', '♥', '♦', '♣'];
+    let símbolo = Math.floor(Math.random() * palo.length);
+  document.querySelector("#randomSymbol").innerHTML = palo[símbolo] + número[cantidad];
+
+  let número = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+    let cantidad = Math.floor(Math.random() * número.length);
+  document.querySelector("#randomNumber").innerHTML = número[cantidad];
+
 };
