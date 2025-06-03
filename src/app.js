1708e6093407c5c15844e3e8939e42ba93170f01
Laura Montes <laumoen@gmail.com>
2025-06-03 10:43:03 +0000

    firstcommit

--- src/app.js
+++ src/app.js
@@ -2,17 +2,21 @@ import "bootstrap";
 import "./style.css";
 
 window.onload = function() {
-  
+
   let palo = ['♠', '♥', '♦', '♣'];
     let símbolo = Math.floor(Math.random() * palo.length);
-  document.querySelector("#randomSuit").innerHTML = palo[símbolo];
-
-};
-
-window.onload = function() {
+  let elementosPalo = document.getElementsByClassName("palos");
+    for (let i = 0; i < elementosPalo.length; i++) {
+      elementosPalo[i].innerHTML = palo[símbolo];
+        if (palo[símbolo] === '♥' || palo[símbolo] === '♦') {
+          elementosPalo[i].style.color = 'red';
+        } else {
+          elementosPalo[i].style.color = 'black';
+        }
+      }
 
   let número = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
     let cantidad = Math.floor(Math.random() * número.length);
   document.querySelector("#randomNumber").innerHTML = número[cantidad];
-
-}
+ 
+};
